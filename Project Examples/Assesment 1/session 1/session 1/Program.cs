f24d3d207445c9a8e13520fde5164caa0e94432f
Lukephillips11 <100251121+Lukephillips11@users.noreply.github.com>
2022-02-23 16:52:13 +1100

    added project

---
+++ Project Examples/Assesment 1/session 1/session 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string givenName;
+            Console.WriteLine("Hello! What is your name? ");
+                givenName = Console.ReadLine();
+
+            Console.WriteLine("Nice to meet you " + givenName);
+            Console.WriteLine("I hope you are well");
+            Console.WriteLine("Press any key to quit");
+        }
+    }
+}
