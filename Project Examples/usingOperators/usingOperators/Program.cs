10e64262b335831687b26e943c75b784ab151369
Lukephillips11 <100251121+Lukephillips11@users.noreply.github.com>
2022-03-02 16:56:33 +1100

    i have been coding my operators practice

---
+++ Project Examples/usingOperators/usingOperators/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usingOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  int health = 90;
+              int healthBonus = 10;
+              Console.WriteLine("You have " + health + " health ");
+              health +=
+                  health + healthBonus;
+              Console.WriteLine("You have " + health + " health");
+              Console.ReadKey(); */
+            int score = 0;
+            int pointValue = 5;
+            int combo = 5;
+            //Start of the game
+            combo++;
+            Console.WriteLine("The player's score is: " + score + " points");
+            score += pointValue * combo;
+            Console.WriteLine("You scored! new score is: " + score + " points");
+            combo++;
+            score += pointValue * combo;   
+            Console.WriteLine("You scored! new score is: " + score + " points");
+            Console.ReadKey();
+        }
+    }
+}
