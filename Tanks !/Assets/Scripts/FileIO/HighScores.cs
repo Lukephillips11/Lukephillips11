9e24b20c388ec0a8c45c4a0ead99588abd113d03
Luke Phillips <s215214@students.aie.edu.au>
2022-10-12 16:58:45 +1100

    Tank
    I continued to complete my tank game

---
+++ Tanks !/Assets/Scripts/FileIO/HighScores.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScores : MonoBehaviour { 
+ 
+       public int[] scores = new int[10]; 
+ 
+       string currentDirectory; 
+      
+       public string scoreFileName = "highscores.txt"; 
+      
+       void Start () {  
+       } 
+ 
+       void Update() { 
+       }  
+ 
+       public void LoadScoresFromFile() { 
+       } 
+ 
+       public void SaveScoresToFile() { 
+       } 
+ 
+       public void AddScore(int newScore) { 
+       } 
+      } 
+
+   
+
+
+
+
+
+
+
+
+
+   
+    
